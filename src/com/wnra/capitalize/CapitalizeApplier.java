cac51f3a8825c9ddcf0777a4172e52b511056281
wnra <wilson.almeida@gft.com>
2021-12-23 12:30:36 -0300

    Quick fixes

--- src/com/wnra/capitalize/CapitalizeApplier.java
+++ src/com/wnra/capitalize/CapitalizeApplier.java
@@ -11,18 +11,17 @@ public class CapitalizeApplier {
 
 	public static <T> void injectFormatting(T objeto) {
 		Class<?> classe = objeto.getClass();
-		List <Field> camposDeclarados = new ArrayList<>();
+		List<Field> camposDeclarados = new ArrayList<>();
 
 		if (objeto.getClass().getSuperclass() != Object.class) {
 			camposDeclarados.addAll(List.of(classe.getSuperclass().getDeclaredFields()));
 		}
-		
+
 		camposDeclarados.addAll(List.of(classe.getDeclaredFields()));
-		
+
 		var camposCapitalize = camposDeclarados.stream()
 				.filter(field -> field.isAnnotationPresent(Capitalize.class) && field.getGenericType() == String.class)
 				.collect(Collectors.toList());
-		System.out.println("Campos declarados: " + camposCapitalize.size());
 
 		camposCapitalize.forEach(field -> {
 			try {
@@ -33,23 +32,30 @@ public class CapitalizeApplier {
 			}
 		});
 	}
-	
-	public static <T> void injectFormatting(T ...objetos) {
-		for (var objeto : objetos) {
-		Class<?> classe = objeto.getClass();
-		var camposDeclarados = List.of(classe.getDeclaredFields());
-		var camposCapitalize = camposDeclarados.stream()
-				.filter(field -> field.isAnnotationPresent(Capitalize.class) && field.getGenericType() == String.class)
-				.collect(Collectors.toList());
 
-		camposCapitalize.forEach(field -> {
-			try {
-				field.setAccessible(true);
-				field.set(objeto, Formatacoes.capitalize(field.get(objeto).toString()));
-			} catch (Exception e) {
-				e.printStackTrace();
+	public static <T> void injectFormatting(T... objetos) {
+		for (var objeto : objetos) {
+			Class<?> classe = objeto.getClass();
+			List<Field> camposDeclarados = new ArrayList<>();
+
+			if (objeto.getClass().getSuperclass() != Object.class) {
+				camposDeclarados.addAll(List.of(classe.getSuperclass().getDeclaredFields()));
 			}
-		});
+
+			camposDeclarados.addAll(List.of(classe.getDeclaredFields()));
+
+			var camposCapitalize = camposDeclarados.stream().filter(
+					field -> field.isAnnotationPresent(Capitalize.class) && field.getGenericType() == String.class)
+					.collect(Collectors.toList());
+
+			camposCapitalize.forEach(field -> {
+				try {
+					field.setAccessible(true);
+					field.set(objeto, Formatacoes.capitalize(field.get(objeto).toString()));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
 		}
 	}
 
